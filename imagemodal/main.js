4ec46f691dc07baf31e10c05606e07c15854b460
thejayadad <thejayadad@gmail.com>
2023-09-16 08:21:04 -0400

    update

---
+++ imagemodal/main.js
@@ -0,0 +1,26 @@
+// Get references to the gallery items and the modal
+const galleryItems = document.querySelectorAll('.gallery-item');
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+const closeModalButton = document.getElementById('close-modal');
+
+galleryItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        openModal(index);
+    });
+});
+
+function openModal(index) {
+    modalImage.src = galleryItems[index].querySelector('img').src;
+    modal.style.display = 'block';
+}
+
+closeModalButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
