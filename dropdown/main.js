32451468415a77b2259544ff54db679820466e22
thejayadad <thejayadad@gmail.com>
2023-09-19 18:22:06 -0400

    update

--- dropdown/main.js
+++ dropdown/main.js
@@ -1,23 +1,15 @@
 const students = ["Jazz", "Jada", "Jace", "Jack", "Eddie"];
 
-const dropdownContent = document.getElementById("dropdown-content");
-
+const dropdownContent = document.getElementById('dropdown-content')
 students.forEach((item) => {
-    const dropdownItem = document.createElement("a");
+    const dropdownItem = document.createElement('a');
     dropdownItem.textContent = item;
     dropdownItem.className = "dropdown-item";
     dropdownContent.appendChild(dropdownItem);
-});
 
-dropdownContent.addEventListener("click", (event) => {
-    const selected = event.target;
-    if (selected.classList.contains("dropdown-item")) {
-        alert(`You selected: ${selected.textContent}`);
-        dropdownContent.style.display = "none";
-    }
-});
+})
 
-const dropdownButton = document.querySelector(".dropdown-button");
+const dropdownButton  = document.querySelector(".dropdown-button");
 dropdownButton.addEventListener("click", () => {
-    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
-});
+    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none": "block";
+})
