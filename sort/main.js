491afeca9486fa1baf60fad391ab9cc489f5fd71
thejayadad <thejayadad@gmail.com>
2023-09-20 18:03:40 -0400

    update

--- sort/main.js
+++ sort/main.js
@@ -1,59 +1,25 @@
-const data = [
-    { name: 'Jackson', age: 10, position: 'Lineman' },
-    { name: 'Brendon', age: 11, position: 'Athlete' },
-    { name: 'Jerome', age: 10, position: 'Athlete' },
-    { name: 'Camden', age: 11, position: 'Lineman' },
-    { name: 'Richard', age: 10, position: 'Lineman' },
-    { name: 'Justin', age: 10, position: 'Lineman' },
-    { name: 'Chris', age: 11, position: 'Athlete' },
+function sortTable(columnIndex) {
+    const table = document.getElementById("sortable-table");
+    const rows = Array.from(table.rows).slice(1);
 
+    const isAscending = table.rows[0].cells[columnIndex].classList.contains("asc");
 
-];
+    for (let i = 0; i < table.rows[0].cells.length; i++) {
+        table.rows[0].cells[i].classList.remove("asc", "desc");
+    }
 
-const sortColumnSelect = document.getElementById('sortColumn');
-const sortAscendingButton = document.getElementById('sortAscending');
-const sortDescendingButton = document.getElementById('sortDescending');
-const tableBody = document.getElementById('tableBody');
-let currentSortColumn = 'name';
-let isAscending = true;
+    table.rows[0].cells[columnIndex].classList.toggle(isAscending ? "desc" : "asc");
 
-function renderTable(data) {
-    tableBody.innerHTML = '';
-    data.forEach(item => {
-        const row = document.createElement('tr');
-        row.innerHTML = `<td>${item.name}</td><td>${item.age}</td><td>${item.position}</td>`;
-        tableBody.appendChild(row);
+    rows.sort((a, b) => {
+        const aValue = a.cells[columnIndex].textContent;
+        const bValue = b.cells[columnIndex].textContent;
+        return isAscending ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
     });
+
+    const tbody = table.tBodies[0];
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+    rows.forEach(row => tbody.appendChild(row));
 }
-
-renderTable(data);
-
-function sortData(column, ascending) {
-    const sortedData = [...data];
-    sortedData.sort((a, b) => {
-        const valueA = a[column];
-        const valueB = b[column];
-        if (valueA < valueB) return ascending ? -1 : 1;
-        if (valueA > valueB) return ascending ? 1 : -1;
-        return 0;
-    });
-    return sortedData;
-}
-
-sortAscendingButton.addEventListener('click', () => {
-    isAscending = true;
-    const sortedData = sortData(currentSortColumn, isAscending);
-    renderTable(sortedData);
-});
-
-sortDescendingButton.addEventListener('click', () => {
-    isAscending = false;
-    const sortedData = sortData(currentSortColumn, isAscending);
-    renderTable(sortedData);
-});
-
-sortColumnSelect.addEventListener('change', () => {
-    currentSortColumn = sortColumnSelect.value;
-    const sortedData = sortData(currentSortColumn, isAscending);
-    renderTable(sortedData);
-});
