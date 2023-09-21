cfb58caf8032588bbc811597d1bb2bad0da72a47
thejayadad <thejayadad@gmail.com>
2023-09-21 13:11:12 -0400

    update

--- clock/main.js
+++ clock/main.js
@@ -1,11 +1,12 @@
-function updateClock() {
+
+function updateClock(){
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2,'0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2,'0');
     const timeString = `${hours}:${minutes}:${seconds}`;
     document.getElementById('clock').textContent = timeString;
-  }
 
-  setInterval(updateClock, 1000);
-  updateClock();
+}
+setInterval(updateClock, 1000);
+updateClock();
