9820cb65878283ecb40db548b55b7d39adec4a54
thejayadad <thejayadad@gmail.com>
2023-09-21 09:05:07 -0400

    update

---
+++ random/main.js
@@ -0,0 +1,15 @@
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeColor() {
+    const button = document.querySelector('.button');
+    const color = getRandomColor();
+    button.style.backgroundColor = color;
+    document.getElementById('colorCode').textContent = `Color Code: ${color}`;
+}
